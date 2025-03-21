40e23ff515dda587110f6349c720c85faff725ab
makowithyou <mako.withyou@gmail.com>
2025-03-21 14:24:18 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+addEventListener(
+    "click", (e) => {
+        e.preventDefault();
+    }
+)
+
+addEventListener(
+    "mouseover", (e) => {
+        toggleAttributeOpen(e);
+    }
+);
+addEventListener(
+    "mouseout", (e) => {
+        toggleAttributeOpen(e);
+    }
+);
+
+function toggleAttributeOpen(e) {
+    if (e.target.tagName == "SUMMARY") {
+        const details = e.target.parentElement;
+        details.toggleAttribute("open");
+    }
+}
